d54e3ed2afc03446a13415b929ff1fdf044f7f8e
obonyodorice <obonyodorice3@gmail.com>
2025-07-21 16:04:06 -0700

    modified models

--- members/models.py
+++ members/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='member_profile')
+    bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    x_url = models.CharField(max_length=255, null=True, blank=True)  # formerly Twitter
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    pinterest_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+    def get_absolute_url(self):
+        return reverse('show_profile_page', kwargs={'pk': self.pk})
