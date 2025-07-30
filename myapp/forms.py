fa12f3408ffcdd269f1fa2e861259e4d12e7f999
obonyodorice <obonyodorice3@gmail.com>
2025-07-30 12:16:50 -0700

    cm

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,53 +1,44 @@
 from django import forms
-from .models import Post, Category, Comment
-
-
-
-choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment')]
-# choices = Category.objects.all().values_list('name', 'name')
-choice_list = []
-
-for item in choices:
-    choice_list.append(item)
-
+from .models import Post, Comment, Newsletter
 
 class PostForm(forms.ModelForm):
+    """Form for creating/editing posts"""
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
-
+        fields = [
+            'title', 'category', 'content', 'excerpt', 
+            'featured_image', 'tags', 'status', 'is_featured'
+        ]
         widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class':'form-control'}),
-            'author': forms.TextInput(attrs={'class':'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
-            #'author': forms.Select(attrs={'class':'form-control'}),
-            'category': forms.Select(choices=choice_list,attrs={'class':'form-control'}),
-            'body': forms.Textarea(attrs={'class':'form-control'}),
-            'snippet': forms.Textarea(attrs={'class':'form-control'}),
-        } 
-
-
-class EditForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'title_tag', 'category', 'body', 'snippet')
-
-        widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class':'form-control'}),
-            'category': forms.Select(choices=choice_list,attrs={'class':'form-control'}),
-            #'author': forms.Select(attrs={'class':'form-control'}),
-            'body': forms.Textarea(attrs={'class':'form-control'}),
-            'snippet': forms.Textarea(attrs={'class':'form-control'}),
-        } 
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'excerpt': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+            'tags': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter tags separated by commas'
+            }),
+        }
 
 class CommentForm(forms.ModelForm):
+    """Form for adding comments"""
     class Meta:
         model = Comment
-        fields = ('name', 'body')
-
+        fields = ['content']
         widgets = {
-            'name': forms.TextInput(attrs={'class':'form-control'}),  
-            'body': forms.Textarea(attrs={'class':'form-control'}),
-            
-        } 
+            'content': forms.Textarea(attrs={
+                'rows': 4,
+                'class': 'form-control',
+                'placeholder': 'Write your comment here...'
+            })
+        }
+
+class NewsletterForm(forms.ModelForm):
+    """Newsletter subscription form"""
+    class Meta:
+        model = Newsletter
+        fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your email'
+            })
+        }
